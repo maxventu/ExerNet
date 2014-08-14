8ac49378c7905e5b0ea9e449667a7f2e6e3b780f
maxventu <nik.maximka@gmail.com>
2014-08-14 20:03:00 +0300

    Beauty ver. 1.1
    Added image uploading

---
+++ Scripts/infinite-scrolling.js
@@ -0,0 +1,17 @@
+﻿$(window).scroll(function()
+{
+        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+            $('div#loadmoreajaxloader').show();
+            $.ajax({
+                url: "User/Details",
+                success: function (html) {
+                    if (html) {
+                        $("#postswrapper").append(html);
+                        $('div#loadmoreajaxloader').hide();
+                    } else {
+                        $('div#loadmoreajaxloader').html('<center>No more posts to show.</center>');
+                    }
+                }
+            });
+        }
+});
